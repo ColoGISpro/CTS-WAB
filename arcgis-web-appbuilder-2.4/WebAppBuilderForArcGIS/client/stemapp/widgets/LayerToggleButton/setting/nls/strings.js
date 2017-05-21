0f635f6309600fc7cec020b5bd1526e92e989e08
ColoGISpro <eric.drummond@state.co.us>
2017-05-21 16:41:00 -0600

    Revert "Deploy Custom Widgets"
    This reverts commit 491cc12ad7c2c5c72446ec7ff504b8b321ceb4bf.

--- arcgis-web-appbuilder-2.4/WebAppBuilderForArcGIS/client/stemapp/widgets/LayerToggleButton/setting/nls/strings.js
+++
@@ -1,6 +0,0 @@
-define({
-  root: ({
-    layerSelectorTitle: "Choose which layers will be toggled by the button.",
-    instructions: "Choose a layer to toggle. Optionally change the widgets name to the text you want to appear in the button tooltip. Optionally change the widgets icon as desired."
-  })
-});
